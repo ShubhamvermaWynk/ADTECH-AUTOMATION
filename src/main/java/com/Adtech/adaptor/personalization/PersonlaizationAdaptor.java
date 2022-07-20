27c654cefeccc0b0a72118a1a241165be1bd9344
shubham verma <40784990+ShubhamvermaWynk@users.noreply.github.com>
2022-07-20 12:35:18 +0530

    COMMENT

--- src/main/java/com/Adtech/adaptor/personalization/PersonlaizationAdaptor.java
+++ src/main/java/com/Adtech/adaptor/personalization/PersonlaizationAdaptor.java
@@ -3,6 +3,7 @@ package com.Adtech.adaptor.personalization;
 import io.restassured.response.Response;
 
 public interface PersonlaizationAdaptor {
+	//Comment
 	
 	Response getOrderList(String userId);
 	Response vmaxCampiagndata();
